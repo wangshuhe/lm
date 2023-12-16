a27b58b0148930d4e9bb7c70dc9dbb298ba02949
王淑禾 <wangshuhe@Mac0114.local>
2023-12-16 14:20:44 +0800

    1

--- exercises/congestion/send.py
+++ exercises/congestion/send.py
@@ -25,7 +25,7 @@ def main():
 
     iface = get_if()
 
-    for i from 1 to 10:
+    for i in range(1, 10):
         pkt =  Ether(src=get_if_hwaddr(iface), dst='08:00:00:00:01:00')
         pkt = pkt / IPv6(dst='1000:0:0:0:0:0:0:1') / Idp(dstSeaid=0x1)  / Common(version=0x1) / SeadpData(flags=0x1, packet_number=i) / args.message
     
